6b3d7ff73bf72a6e39ac0d29d1b5e3d6c2cd4d39
youngwooooo <dlddudn02@gmail.com>
2021-12-22 20:47:49 +0900

    공통 Service 수정

--- src/main/java/com/work/daily/access/service/AccessService.java
+++ src/main/java/com/work/daily/access/service/AccessService.java
@@ -1,18 +1,39 @@
 package com.work.daily.access.service;
 
-import com.work.daily.access.repository.AccessRepository;
+import com.work.daily.access.dto.JoinUserDto;
+import com.work.daily.domain.UserRole;
+import com.work.daily.domain.entity.User;
+import com.work.daily.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-@Slf4j
-@RequiredArgsConstructor
-@Service
-public class AccessService {
-    private final AccessRepository accessRepository;
+import javax.transaction.Transactional;
+import java.util.Optional;
 
-    public String AccessMain(String userId, String userPassword) {
-        log.info("AccessController::AccessMain Access...");
-        return "OK";
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AccessService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    // 회원 가입
+    @Transactional
+    public void save(JoinUserDto dto){
+        Optional<User> checkUser = userRepository.findById(dto.getId());
+
+        if(!checkUser.isPresent()){
+            dto.setPassword(bCryptPasswordEncoder.encode(dto.getPassword()));
+            dto.setRole(UserRole.USER);
+
+            userRepository.save(dto.toEntity());
+            log.info("save 성공");
+        }else {
+            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+        }
+
     }
 }
