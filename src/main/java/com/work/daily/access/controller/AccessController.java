bca8539b484ceba6d70e1358e48ada28106a5d54
youngwooooo <dlddudn02@gmail.com>
2021-12-22 20:47:15 +0900

    공통 Controller 수정

--- src/main/java/com/work/daily/access/controller/AccessController.java
+++ src/main/java/com/work/daily/access/controller/AccessController.java
@@ -1,28 +1,25 @@
 package com.work.daily.access.controller;
 
-import com.work.daily.access.service.AccessService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@Slf4j
-@RequiredArgsConstructor
 @Controller
 public class AccessController {
 
-    private final AccessService accessService;
-
-    @RequestMapping(name = "/main")
-    public void AccessMain() {
-        String userId = "";
-        String userPassword = "";
-
-        log.info("AccessController::AccessMain Access...");
-
-        String s = accessService.AccessMain(userId, userPassword);
-        System.out.println("s = " + s);
-
+    @GetMapping("/")
+    public String index(){
+        return "contents/common/index";
     }
-}
 
+    @GetMapping("/login")
+    public String loginForm(){
+        return "contents/common/login";
+    }
+
+    @GetMapping("/join")
+    public String joinForm(){
+        return "contents/common/join";
+    }
+
+}
