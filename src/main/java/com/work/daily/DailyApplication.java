0488224abe9f32bf654e83198eb6f6046d68fb95
youngwooooo <dlddudn02@gmail.com>
2021-12-22 20:49:57 +0900

    @EnableJpaAuditing 추가

--- src/main/java/com/work/daily/DailyApplication.java
+++ src/main/java/com/work/daily/DailyApplication.java
@@ -2,7 +2,9 @@ package com.work.daily;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DailyApplication {
 
