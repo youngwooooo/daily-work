4bfa5066b4a0b6e41dcc49ef5d91c55834fc94c8
youngwooooo <dlddudn02@gmail.com>
2021-12-22 20:57:38 +0900

    Spring Security Conifg - 인증 및 권한 구현

---
+++ src/main/java/com/work/daily/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.work.daily.config;
+
+import com.work.daily.domain.UserRole;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/user/**").authenticated()
+                .antMatchers("/admin/**").hasRole(UserRole.ADMIN.getValue())
+                .anyRequest().permitAll()
+            .and()
+                .formLogin()
+                .loginPage("/login");
+    }
+}
