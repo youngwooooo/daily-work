57b0ad76715bce0a99c87a52c8911c0bb4f59c80
youngwooooo <dlddudn02@gmail.com>
2021-12-22 20:58:10 +0900

    User Repository

---
+++ src/main/java/com/work/daily/domain/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.work.daily.domain.repository;
+
+import com.work.daily.domain.entity.User;
+import org.hibernate.metamodel.model.convert.spi.JpaAttributeConverter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findById(String id);
+}
