79c1af3e375420801fe9a1a6660110729f9b6a44
youngwooooo <dlddudn02@gmail.com>
2021-12-27 13:33:17 +0900

    form 회원가입 서비스 구현

--- src/main/java/com/work/daily/access/service/AccessService.java
+++ src/main/java/com/work/daily/access/service/AccessService.java
@@ -1,5 +1,6 @@
 package com.work.daily.access.service;
 
+import com.work.daily.access.ReturnResult;
 import com.work.daily.access.dto.UserDto;
 import com.work.daily.domain.UserRole;
 import com.work.daily.domain.entity.User;
@@ -20,22 +21,27 @@ public class AccessService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    // 회원 가입
+    /**
+     * 회원가입
+     * @param userDto
+     * @return SUCCESS, FAIL
+     */
     @Transactional
-    public void save(UserDto userDto){
+    public String save(UserDto userDto){
+        // userDto.getId()로 회원 찾기
         Optional<User> findUser = userRepository.findById(userDto.getId());
-
-        if(!findUser.isPresent()){
-            userDto.setPassword(bCryptPasswordEncoder.encode(userDto.getPassword()));
-            userDto.setRole(UserRole.USER);
-
-            userRepository.save(userDto.toEntity());
-            log.info("save 성공");
-        }else {
-            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+        // 회원이 존재할 때
+        if(findUser.isPresent()){
+            return ReturnResult.FAIL.getValue();
         }
 
-    }
+        userDto.setPassword(bCryptPasswordEncoder.encode(userDto.getPassword()));
+        userDto.setRole(UserRole.USER);
 
+        userRepository.save(userDto.toEntity());
+        log.info("save() : 회원가입 성공");
+
+        return ReturnResult.SUCCESS.getValue();
+    }
 
 }
