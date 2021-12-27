5a2cf06a1c9902937cc34ce2b8f65fe9be654b7c
youngwooooo <dlddudn02@gmail.com>
2021-12-27 23:44:22 +0900

    구글, 카카오, 네이버로 로그인 클릭 시 실행 로직 구현

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,3 +1,13 @@
 $(function(){
+    $("#login-social-google").on("click", function(){
+        location.href="/oauth2/authorization/google";
+    });
 
+    $("#login-social-kakao").on("click", function(){
+        location.href="/oauth2/authorization/kakao";
+    });
+
+    $("#login-social-naver").on("click", function(){
+        location.href="/oauth2/authorization/naver";
+    });
 });
