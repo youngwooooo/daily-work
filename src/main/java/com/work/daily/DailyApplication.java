7d20c2989160ac52a23b2c1e06625d4a444c1821
youngwooooo <dlddudn02@gmail.com>
2021-12-27 23:35:19 +0900

    BCryptPasswordEncoder를 application 실행 시 bean으로 등록하게 함 (SecurityConfig에 bean 등록 시, 순환참조 발생)

--- src/main/java/com/work/daily/DailyApplication.java
+++ src/main/java/com/work/daily/DailyApplication.java
@@ -2,11 +2,19 @@ package com.work.daily;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class DailyApplication {
+
+    // 해쉬 암호화
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DailyApplication.class, args);
