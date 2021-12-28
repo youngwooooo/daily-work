fba4edd79890d6a14274a56831c590d3e24be6db
youngwooooo <dlddudn02@gmail.com>
2021-12-28 22:31:21 +0900

    @GetMapping("/login") -> @RequestMapping("/login")로 변경

--- src/main/java/com/work/daily/access/controller/AccessController.java
+++ src/main/java/com/work/daily/access/controller/AccessController.java
@@ -1,9 +1,11 @@
 package com.work.daily.access.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class AccessController {
 
@@ -12,7 +14,7 @@ public class AccessController {
         return "contents/common/index";
     }
 
-    @GetMapping("/login")
+    @RequestMapping("/login")
     public String loginForm(){
         return "contents/common/login";
     }
