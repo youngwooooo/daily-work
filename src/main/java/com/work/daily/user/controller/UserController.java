18ffc7e56c120b5baeb7b66437579528bf881392
youngwooooo <dlddudn02@gmail.com>
2022-01-01 22:41:30 +0900

    주석 수정 비밀번호 변경 성공 페이지 추가

--- src/main/java/com/work/daily/user/controller/UserController.java
+++ src/main/java/com/work/daily/user/controller/UserController.java
@@ -11,14 +11,32 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class UserController {
 
+    /**
+     * 계정관리(마이페이지) 화면
+     * @return 계정관리(마이페이지) 화면
+     */
     @GetMapping("/user/mypage")
     public String mypageForm(){
         return "contents/user/mypage";
     }
 
+    /**
+     * 계정관리(마이페이지) - 비밀번호 확인 - 비밀번호 변경 화면
+     * @return 비밀번호 변경 화면
+     */
     @GetMapping("/user/mypage/password")
     public String modifyPasswordForm(){
         return "contents/user/password";
     }
+
+    /**
+     * 계정관리(마이페이지) - 비밀번호 확인 - 비밀번호 변경 - 비밀번호 변경 완료 화면
+     * @return 비밀번호 변경 완료 화면
+     */
+    @GetMapping("/user/mypage/password/success")
+    public String modifyPasswordSuccess(){
+        return "contents/user/modifyPasswordSuccess";
+    }
+
 
 }
