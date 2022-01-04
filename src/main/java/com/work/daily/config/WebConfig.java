88d71836290d14e99fdd07540ee138ac45f3a8fb
youngwooooo <dlddudn02@gmail.com>
2022-01-04 17:45:40 +0900

    서버에 저장된 파일들을 웹에서 접근할 수 있도록 설정하는 WebConfig 생성

---
+++ src/main/java/com/work/daily/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.work.daily.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 서버에 저장된 파일들을 웹에서 접근할 수 있도록 설정
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${custom.path.profile-image}")
+    private String profileUploadPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        /*
+            addResourceHandler() : 화면에서 접근하는 url 기본 설정 -> http:localhost:8888/profile/**
+            addResourceLocations() : 파일이 저장된 위치 기본 설정
+                                     "file:///"을 앞에 붙여줘야 파일로 인식한다.
+         */
+        registry.addResourceHandler("/profile/**")
+                .addResourceLocations("file:///" + profileUploadPath);
+    }
+}
