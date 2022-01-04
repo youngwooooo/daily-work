ec7133816bdb796ce8d64ab65f5d5991418f252a
Lee JunBeon <skier5000@naver.com>
2022-01-04 20:40:54 +0900

    미션현황 제외 모든 entity 생성 완료

---
+++ src/main/java/com/work/daily/domain/entity/BoardComment.java
@@ -0,0 +1,46 @@
+package com.work.daily.domain.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+/**
+ * 게시판 댓글
+ */
+
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "TB_BOARD_COMMENT")
+public class BoardComment extends BaseTime {
+
+    @Id
+    @Column(columnDefinition = "varchar(20) comment '댓글번호'")
+    private String commentSeq;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumns({
+            @JoinColumn(name = "USER_SEQ"),
+            @JoinColumn(name = "USER_ID")
+    })
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumns({
+            @JoinColumn(name = "BOARD_SEQ")
+    })
+    private Board board;
+
+    @Column(columnDefinition = "varchar(60) comment '부모댓글번호'")
+    private String parentCommentSeq;
+
+    @Column(columnDefinition = "varchar(300) comment '댓글내용'")
+    private String commentDesc;
+
+    @Column(columnDefinition = "varchar(10) comment '삭제여부'")
+    private String delYn;
+
+}
