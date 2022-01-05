273ea34bd40d410717e389379084f3524788b593
youngwooooo <dlddudn02@gmail.com>
2022-01-05 18:44:24 +0900

    생성자, 수정자 자동입력을 위한 @CreatedBy, @LastModifiedBy 추가

--- src/main/java/com/work/daily/domain/entity/BaseTime.java
+++ src/main/java/com/work/daily/domain/entity/BaseTime.java
@@ -1,16 +1,18 @@
 package com.work.daily.domain.entity;
 
 import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Column;
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseTime {
@@ -19,13 +21,15 @@ public class BaseTime {
     @Column(updatable = false, columnDefinition = "TEXT COMMENT '생성일시'")
     private LocalDateTime insDtm;
 
-    @Column(columnDefinition = "TEXT COMMENT '생성자'")
+    @CreatedBy
+    @Column(updatable = false, columnDefinition = "TEXT COMMENT '생성자'")
     private String insUser;
 
     @LastModifiedDate
     @Column(columnDefinition = "TEXT COMMENT '수정일시'")
-    private String updDtm;
+    private LocalDateTime updDtm;
 
+    @LastModifiedBy
     @Column(columnDefinition = "TEXT COMMENT '수정자'")
     private String updUser;
 
