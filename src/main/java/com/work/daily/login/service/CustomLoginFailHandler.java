14d8f43d5d2794455e952be400af83b815bcc4d6
youngwooooo <dlddudn02@gmail.com>
2022-01-05 21:12:04 +0900

    중복 로그인 에러(SessionAuthenticationException) 처리 추가

--- src/main/java/com/work/daily/login/service/CustomLoginFailHandler.java
+++ src/main/java/com/work/daily/login/service/CustomLoginFailHandler.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.session.SessionAuthenticationException;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.ServletException;
@@ -31,6 +32,8 @@ public class CustomLoginFailHandler implements AuthenticationFailureHandler {
             errorMsg = "존재하는 회원이 아닙니다.";
         }else if(exception instanceof BadCredentialsException){
             errorMsg = "아이디 또는 비밀번호를 확인하세요.";
+        }else if(exception instanceof SessionAuthenticationException){
+            errorMsg = "이미 로그인한 회원입니다. 로그아웃 후 다시 로그인 해주세요.";
         }else {
             errorMsg = "알 수 없는 오류로 로그인 할 수 없습니다. 관리자에게 문의하세요";
         }
