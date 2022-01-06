5cdfae0c0bbe5ee5b107d293685710006afcc5a7
youngwooooo <dlddudn02@gmail.com>
2022-01-06 22:32:53 +0900

    주석 추가 index() loginForm() 수정

--- src/main/java/com/work/daily/access/controller/AccessController.java
+++ src/main/java/com/work/daily/access/controller/AccessController.java
@@ -1,6 +1,8 @@
 package com.work.daily.access.controller;
 
+import com.work.daily.login.auth.CustomUserDetails;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,25 +10,52 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 @Controller
 public class AccessController {
-
+    /**
+     * 메인 페이지
+     * @param customUserDetails
+     * @return 로그인 : 메인 페이지 VIEW
+     *         비로그인 : 전체 미션 VIEW
+     */
     @GetMapping({"","/"})
-    public String index(){
+    public String index(@AuthenticationPrincipal CustomUserDetails customUserDetails){
         log.info("AccessController::index called");
+
+        if(customUserDetails != null){
+            return "contents/common/index";
+        }
+        return "contents/mission/missions";
+    }
+
+    /**
+     * 로그인 페이지
+     * @param customUserDetails
+     * @return 로그인 : 메인 페이지 VIEW(URL 접속을 막기 위함)
+     *         비로그인 : 로그인 페이지 VIEW
+     */
+    @RequestMapping("/login")
+    public String loginForm(@AuthenticationPrincipal CustomUserDetails customUserDetails){
+        log.info("AccessController::loginForm called");
+
+        if(customUserDetails == null){
+            return "contents/common/login";
+        }
         return "contents/common/index";
     }
 
-    @RequestMapping("/login")
-    public String loginForm(){
-        log.info("AccessController::loginForm called");
-        return "contents/common/login";
-    }
-
+    /**
+     * 회원가입 페이지
+     * @return 회원가입 VIEW
+     */
     @GetMapping("/join")
     public String joinForm(){
         log.info("AccessController::joinForm called");
         return "contents/common/join";
     }
 
+    /**
+     * 회원가입 성공 페이지
+     * @return 회원가입 성공 VIEW
+     */
     @GetMapping("/join/success")
     public String joinSuccess(){
         log.info("AccessController::joinSuccess called");
