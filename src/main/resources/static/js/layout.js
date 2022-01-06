5ad84239ff2661f79119a558f42d8e67f860bb5e
youngwooooo <dlddudn02@gmail.com>
2022-01-06 22:41:08 +0900

    CKEditor5 CDN 추가 전체 MISSION 클릭 시 로직 추가

--- src/main/resources/static/js/layout.js
+++ src/main/resources/static/js/layout.js
@@ -1,15 +1,23 @@
 $(function(){
-    // header 메뉴
+    /* header */
+    // [로그인] 클릭 시
     $("#btn-move-login-form").on("click", function(){
         location.href = "/login";
     });
 
+    // [회원가입] 클릭 시
     $("#btn-move-join-form").on("click", function(){
         location.href = "/join";
     });
 
+    // [로그아웃] 클릭 시
     $("#btn-move-logout").on("click", function(){
         location.href = "/logout";
+    });
+
+    // 회원 - [전체 MISSION] 클릭 시
+    $("#btn-move-all-mission").on("click", function(){
+        location.href = "/missions"
     });
 
 });
