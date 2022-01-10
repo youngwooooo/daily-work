78009f9e34699a7170542c5cc26fbcf9148157f1
youngwooooo <dlddudn02@gmail.com>
2022-01-10 21:45:28 +0900

    RequestMissionDto, ResponseMissionDto 생성

---
+++ src/main/java/com/work/daily/mission/dto/RequestMissionDto.java
@@ -0,0 +1,68 @@
+package com.work.daily.mission.dto;
+
+import com.work.daily.domain.entity.Mission;
+import com.work.daily.domain.entity.User;
+import lombok.*;
+
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RequestMissionDto {
+
+    @NotNull(message = "회원 정보가 존재하지 않습니다.")
+    private User user;
+
+    @NotBlank(message = "미션 제목을 입력해주세요.")
+    private String missionNm;
+
+    @NotBlank(message = "미션에 대한 설명 또는 소개를 입력해주세요.")
+    private String missionDesc;
+
+    @NotNull(message = "미션 시작일을 설정해주세요.")
+    private LocalDateTime missionStDt;
+
+    @NotNull(message = "미션 종료일을 설정해주세요.")
+    @Future(message = "미션 종료일은 현재 시간 이후여야 합니다.")
+    private LocalDateTime missionEndDt;
+
+    @NotBlank(message = "공개 여부를 설정해주세요.")
+    private String releaseYn;
+
+    @NotBlank(message = "자동참여 여부를 설정해주세요")
+    private String autoAccessYn;
+
+    @NotBlank(message = "방장 여부를 설정해주세요")
+    private String masterYn;
+
+    @NotBlank(message = "삭제 여부를 설정해주세요")
+    private String delYn;
+
+    @NotBlank(message = "임시 여부를 설정해주세요")
+    private String temporaryYn;
+
+    private String missionImage;
+
+    public Mission toEntity(){
+        return Mission.builder()
+                        .user(user)
+                        .missionNm(missionNm)
+                        .missionDesc(missionDesc)
+                        .missionStDt(missionStDt)
+                        .missionEndDt(missionEndDt)
+                        .releaseYn(releaseYn)
+                        .autoAccessYn(autoAccessYn)
+                        .masterYn(masterYn)
+                        .delYn(delYn)
+                        .temporaryYn(temporaryYn)
+                        .missionImage(missionImage)
+                        .build();
+    }
+}
---
+++ src/main/java/com/work/daily/mission/dto/ResponseMissionDto.java
@@ -0,0 +1,45 @@
+package com.work.daily.mission.dto;
+
+import com.work.daily.domain.entity.Mission;
+import com.work.daily.domain.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseMissionDto {
+
+    private Long missionSeq;
+    private User user;
+    private String missionNm;
+    private String missionDesc;
+    private LocalDateTime missionStDt;
+    private LocalDateTime missionEndDt;
+    private String releaseYn;
+    private String autoAccessYn;
+    private String masterYn;
+    private String delYn;
+    private String temporaryYn;
+    private String reviewGrade;
+    private String missionImage;
+
+    public ResponseMissionDto(Mission mission){
+        this.missionSeq = mission.getMissionSeq();
+        this.user = mission.getUser();
+        this.missionNm = mission.getMissionNm();
+        this.missionDesc = mission.getMissionDesc();
+        this.missionStDt = mission.getMissionStDt();
+        this.missionEndDt = mission.getMissionEndDt();
+        this.releaseYn = mission.getReleaseYn();
+        this.autoAccessYn = mission.getAutoAccessYn();
+        this.masterYn = mission.getMasterYn();
+        this.delYn = mission.getDelYn();
+        this.temporaryYn = mission.getTemporaryYn();
+        this.reviewGrade = mission.getReviewGrade();
+        this.missionImage = mission.getMissionImage();
+    }
+}
