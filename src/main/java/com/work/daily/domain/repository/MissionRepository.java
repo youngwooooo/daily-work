6e7c51d919a65f99b27a5563dfc6f3834d04568c
youngwooooo <dlddudn02@gmail.com>
2022-01-11 21:59:24 +0900

    MissionRepositoryCustom 추가적으로 extends

--- src/main/java/com/work/daily/domain/repository/MissionRepository.java
+++ src/main/java/com/work/daily/domain/repository/MissionRepository.java
@@ -3,6 +3,6 @@ package com.work.daily.domain.repository;
 import com.work.daily.domain.entity.Mission;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MissionRepository extends JpaRepository<Mission, Long> {
+public interface MissionRepository extends JpaRepository<Mission, Long>, MissionRepositoryCustom {
 
 }
