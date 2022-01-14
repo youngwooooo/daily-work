0fb03309bdf3c2382f704eb8e83d859115669177
youngwooooo <dlddudn02@gmail.com>
2022-01-14 19:38:02 +0900

    MissionRepositoryCustom 패키지 변경으로 import 수정

--- src/main/java/com/work/daily/domain/repository/MissionRepository.java
+++ src/main/java/com/work/daily/domain/repository/MissionRepository.java
@@ -1,6 +1,7 @@
 package com.work.daily.domain.repository;
 
 import com.work.daily.domain.entity.Mission;
+import com.work.daily.domain.repository.custom.MissionRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MissionRepository extends JpaRepository<Mission, Long>, MissionRepositoryCustom {
