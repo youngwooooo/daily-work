0b9a54d14fd552c549f40170f9d24a7531375f47
youngwooooo <dlddudn02@gmail.com>
2022-01-14 19:35:16 +0900

    JpaRepository<User, Long> -> JpaRepository<User, UserPK>로 수정

--- src/main/java/com/work/daily/domain/repository/UserRepository.java
+++ src/main/java/com/work/daily/domain/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package com.work.daily.domain.repository;
 
 import com.work.daily.domain.entity.User;
+import com.work.daily.domain.entity.UserPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UserPK> {
     Optional<User> findByUserId(String userId);
 }
