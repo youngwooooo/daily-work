763333a037a891aa288d3d707155cfe6fa20a1ee
youngwooooo <dlddudn02@gmail.com>
2022-01-16 23:21:50 +0900

    미션 창며하기 클릭 시, 미션 참여자 추가 로직 구현(수정 필요)

--- src/main/resources/static/js/mission/detailMission.js
+++ src/main/resources/static/js/mission/detailMission.js
@@ -6,4 +6,39 @@ $(function(){
     $(".mission-start-date").text(formatMissionStDt);
     $(".mission-end-date").text(formatMissionEndDt);
 
+    $("#btn-join-mission").on("click", function(){
+        alert("미션 참여하기");
+
+        var missionSeq = $("#missionSeq").val();
+        var userSeq = $("#userSeq").val();
+        var userId = $("#userId").val();
+        var missionJoinDt = new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60000).toISOString().slice(0, 16);
+        var missionJoinYn = "Y";
+        var missionJoinApprovalDt = new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60000).toISOString().slice(0, 16);
+
+        var data = {
+                        "missionSeq" : missionSeq
+                        , "userSeq" : userSeq
+                        , "userId" : userId
+                        , "missionJoinDt" : missionJoinDt
+                        , "missionJoinYn" : missionJoinYn
+                        , "missionJoinApprovalDt" : missionJoinApprovalDt
+                   };
+
+        $.ajax({
+            url: "/mission/" + missionSeq + "/join"
+            , type : "post"
+            , data : JSON.stringify(data)
+            , contentType: "application/json; charset=UTF-8"
+            , dataType : "json"
+            , success : function(result){
+
+            }
+            , error : function(xhr){
+
+            }
+        });
+
+    });
+
 });
