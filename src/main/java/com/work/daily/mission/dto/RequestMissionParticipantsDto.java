118c7ae50f8b7486b8ca4866a7f7bc5632dfe078
youngwooooo <dlddudn02@gmail.com>
2022-01-17 21:59:09 +0900

    missionSeq, userSeq, userId 유효성 검사 추가

--- src/main/java/com/work/daily/mission/dto/RequestMissionParticipantsDto.java
+++ src/main/java/com/work/daily/mission/dto/RequestMissionParticipantsDto.java
@@ -3,6 +3,8 @@ package com.work.daily.mission.dto;
 import com.work.daily.domain.entity.MissionParticipants;
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Getter
@@ -13,8 +15,11 @@ import java.time.LocalDateTime;
 @Builder
 public class RequestMissionParticipantsDto {
 
+    @NotNull(message = "미션번호가 존재하지 않습니다.")
     private long missionSeq;
+    @NotNull(message = "회원번호가 존재하지 않습니다.")
     private long userSeq;
+    @NotBlank(message = "회원ID가 존재하지 않습니다.")
     private String userId;
     private LocalDateTime missionJoinDt;
     private String missionJoinYn;
