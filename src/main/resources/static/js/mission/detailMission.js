d4f450c3b9b46a0be610a2711dd826f78c2dbb07
youngwooooo <dlddudn02@gmail.com>
2022-01-17 21:50:47 +0900

    1. 조건에 따른 버튼 보이기 수정 : [미션 수정하기], [미션 삭제하기], [미션 참여하기], [미션 탈퇴하기] 2. [미션 삭제하기] 클릭 시, 미션 삭제여부 Y로 수정 로직 구현 3. [미션 참여하기] 클릭 시, 미션 참여자 추가 로직 구현 4. [미션 탈퇴하기] 클릭 시, 미션 참여자 삭제 로직 구현

--- src/main/resources/static/js/mission/detailMission.js
+++ src/main/resources/static/js/mission/detailMission.js
@@ -6,15 +6,80 @@ $(function(){
     $(".mission-start-date").text(formatMissionStDt);
     $(".mission-end-date").text(formatMissionEndDt);
 
-    $("#btn-join-mission").on("click", function(){
-        alert("미션 참여하기");
+    // 미션 참여자 존재 여부에 따른 [미션 참여하기], [미션 탈퇴하기] 버튼 보이기
+    // 미션 생성자와 로그인 한 회원이 같을 경우
+    if($("#missionCreatedUserId").val() == $("#userId").val()){
+        $("#btn-modify-mission").css("display", "inline-block");
+        $("#btn-delete-mission").css("display", "inline-block");
+    }else {
 
+        var participantsIdInput = $("input[name='participantsId']");
+        for(var i=0; i<participantsIdInput.length; i++){
+            // 미션 참여자
+            var participantsId = participantsIdInput[i].value;
+            // 미션 참여자가 로그인 한 회원과 같을 경우
+            if(participantsId == $("#userId").val()){
+                $("#btn-join-mission").css("display", "none");
+                $("#btn-secession-mission").css("display", "inline-block");
+            }else {
+                $("#btn-join-mission").css("display", "inline-block");
+                $("#btn-secession-mission").css("display", "none");
+            }
+        }
+    }
+
+    /* 미션 상세 조회 - [미션 삭제하기] */
+    $("#btn-delete-mission").on("click", function(){
         var missionSeq = $("#missionSeq").val();
-        var userSeq = $("#userSeq").val();
-        var userId = $("#userId").val();
-        var missionJoinDt = new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60000).toISOString().slice(0, 16);
-        var missionJoinYn = "Y";
-        var missionJoinApprovalDt = new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60000).toISOString().slice(0, 16);
+        var delYn = "Y";
+
+        var data = {
+            "missionSeq" : missionSeq
+            , "delYn" : delYn
+        };
+
+        $.ajax({
+            url : "/mission/" + missionSeq
+            , type : "delete"
+            , data : data
+            , contentType: "application/x-www-form-urlencoded; charset=UTF-8;"
+            , dataType : "json"
+            , success : function(result){
+                if(result.status == "201"){
+                    alert(result.message);
+                    location.href = "/missions";
+                }
+            }
+            , error : function(xhr){
+                console.log(xhr);
+            }
+        });
+    });
+
+
+    /* 미션 상세 조회 - [미션 참여하기] */
+    $("#btn-join-mission").on("click", function(){
+        if($("#userId").val() == null){
+            alert("로그인이 필요합니다.");
+            return;
+        }
+
+        var missionSeq = $("#missionSeq").val(); // 미션번호
+        var userSeq = $("#userSeq").val();       // 회원번호
+        var userId = $("#userId").val();         // 회원ID
+        var missionJoinDt = new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60000).toISOString().slice(0, 16); // 미션참여일
+
+        var autoAccessYn = $("#autoAccessYn").val();    // 자동참여 여부
+        var missionJoinYn = ""; // 참여여부
+        var missionJoinApprovalDt = ""; // 참여승인일
+        // 자동참여 여부에 따른 참여여부 값
+        if(autoAccessYn == "Y"){
+            missionJoinYn = "Y"
+            missionJoinApprovalDt = new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60000).toISOString().slice(0, 16);
+        }else {
+            missionJoinYn = "N";
+            missionJoinApprovalDt = null;
+        }
 
         var data = {
                         "missionSeq" : missionSeq
@@ -32,10 +97,48 @@ $(function(){
             , contentType: "application/json; charset=UTF-8"
             , dataType : "json"
             , success : function(result){
-
+                if(result.status == "201"){
+                    alert(result.message);
+                    location.reload();
+                }
             }
             , error : function(xhr){
+                if(xhr.responseJSON.status = "400"){
+                    alert("이미 참여한 미션입니다.");
+                }
+            }
+        });
 
+    });
+
+    /* 미션 상세 조회 - [미션 탈퇴하기] */
+    $("#btn-secession-mission").on("click", function(){
+        var missionSeq = $("#missionSeq").val();
+        var userSeq = $("#userSeq").val();
+        var userId = $("#userId").val();
+
+        var data = {
+                        "missionSeq" : missionSeq
+                        , "userSeq" : userSeq
+                        , "userId" : userId
+                   };
+
+        $.ajax({
+            url : "/mission/" + missionSeq + "/secession"
+            , type : "delete"
+            , data : JSON.stringify(data)
+            , contentType: "application/json; charset=UTF-8"
+            , dataType : "json"
+            , success : function(result){
+                if(result.status == "200"){
+                    alert(result.message);
+                    location.href = "/missions";
+                }
+            }
+            , error : function(xhr){
+                if(xhr.responseJSON.status = "400"){
+
+                }
             }
         });
 
