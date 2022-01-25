895a5613b0a64b3ffc05f7fa103bd4ad8bcaa636
youngwooooo <dlddudn02@gmail.com>
2022-01-25 22:02:41 +0900

    1. MissionStateRepositoryCustom 상속 추가

--- src/main/java/com/work/daily/domain/repository/MissionStateRepository.java
+++ src/main/java/com/work/daily/domain/repository/MissionStateRepository.java
@@ -2,7 +2,8 @@ package com.work.daily.domain.repository;
 
 import com.work.daily.domain.entity.MissionState;
 import com.work.daily.domain.pk.MissionStatePK;
+import com.work.daily.domain.repository.custom.MissionStateRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MissionStateRepository extends JpaRepository<MissionState, MissionStatePK> {
+public interface MissionStateRepository extends JpaRepository<MissionState, MissionStatePK>, MissionStateRepositoryCustom {
 }
