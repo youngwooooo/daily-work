9e3036ffcce5ce67ac9c787551c161117d3941b0
youngwooooo <dlddudn02@gmail.com>
2022-01-27 22:18:17 +0900

    1. List<MissionState> findAllMissionStateByMissionSeq(long missionSeq) 수정 : 미션 참여자 조인 추가

--- src/main/java/com/work/daily/domain/repository/custom/impl/MissionStateRepositoryCustomImpl.java
+++ src/main/java/com/work/daily/domain/repository/custom/impl/MissionStateRepositoryCustomImpl.java
@@ -2,6 +2,7 @@ package com.work.daily.domain.repository.custom.impl;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.work.daily.domain.entity.MissionState;
+import com.work.daily.domain.entity.QMissionParticipants;
 import com.work.daily.domain.entity.QMissionState;
 import com.work.daily.domain.repository.custom.MissionStateRepositoryCustom;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +17,7 @@ public class MissionStateRepositoryCustomImpl implements MissionStateRepositoryC
     private final JPAQueryFactory jpaQueryFactory;
 
     QMissionState qMissionState = QMissionState.missionState;
+    QMissionParticipants qMissionParticipants = QMissionParticipants.missionParticipants;
 
     /**
      * 미션 번호에 따른 모든 미션 현황 조회
@@ -25,6 +27,8 @@ public class MissionStateRepositoryCustomImpl implements MissionStateRepositoryC
     @Override
     public List<MissionState> findAllMissionStateByMissionSeq(long missionSeq) {
         return jpaQueryFactory.selectFrom(qMissionState)
+                .innerJoin(qMissionState.missionParticipants, qMissionParticipants)
+                .fetchJoin()
                 .where(qMissionState.missionParticipants.missionSeq.eq(missionSeq))
                 .fetch();
     }
