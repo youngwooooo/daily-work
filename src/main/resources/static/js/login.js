b4d03c2925ae96e063f13bab3a246762f0630414
youngwooooo <dlddudn02@gmail.com>
2022-02-04 19:54:40 +0900

    1. 아이디, 비밀번호 input에서 엔터키 누를 시, [로그인] 버튼 누르는 로직 추가

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,4 +1,13 @@
 $(function(){
+
+     // 엔터키로 [로그인] 버튼 누르기
+     $(".login-div #userId, .login-div #userPw").on("keyup",function(key){
+        if(key.keyCode==13) {
+            $("#btn-login").click();
+        }
+     });
+
+    // 일반 로그인 처리
     $("#btn-login").on("click", function(){
         var loginData = {
             "userId" : $("#userId").val()
@@ -25,14 +34,17 @@ $(function(){
 
     }); // end function
 
+    // 구글 로그인 처리
     $("#login-social-google").on("click", function(){
         location.href="/oauth2/authorization/google";
     });
 
+    // 카카오 로그인 처리
     $("#login-social-kakao").on("click", function(){
         location.href="/oauth2/authorization/kakao";
     });
 
+    // 네이버 로그인 처리
     $("#login-social-naver").on("click", function(){
         location.href="/oauth2/authorization/naver";
     });
