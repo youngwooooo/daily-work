77a5c0a59fe86b4ffa0991874663179369b39004
youngwooooo <dlddudn02@gmail.com>
2022-02-10 22:08:45 +0900

    1. index -> myPage로 html, js, css 이름변경

--- src/main/resources/static/js/myPage.js
+++ src/main/resources/static/js/myPage.js
@@ -2,7 +2,7 @@ $(function(){
     /* 회원 */
     // 회원 - [계정관리] 클릭 시
     $("#btn-move-mypage-form").on("click", function(){
-        location.href = "/user/mypage/";
+        location.href = "/user/my-account";
     });
 
 });
