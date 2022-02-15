8373cb03184aa34cc604fca05261be4a14698af1
youngwooooo <dlddudn02@gmail.com>
2022-02-15 20:54:41 +0900

    1. myParticipationMission() 추가 : 나의 참여 미션 controller

--- src/main/java/com/work/daily/user/controller/UserController.java
+++ src/main/java/com/work/daily/user/controller/UserController.java
@@ -1,22 +1,64 @@
 package com.work.daily.user.controller;
 
+import com.work.daily.login.auth.CustomUserDetails;
+import com.work.daily.mission.dto.ResponseMissionDto;
+import com.work.daily.mission.service.MissionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
 @Controller
 public class UserController {
 
+    private final MissionService missionService;
+
     /**
      * 마이페이지 화면
      * @return 마이페이지 VIEW
      */
     @GetMapping("/user/mypage")
-    public String myPageForm() {
+    public String myPage(@AuthenticationPrincipal CustomUserDetails customUserDetails, Model model) {
+        String userId = customUserDetails.getLoginUserDto().getUserId();
+
+        // 최근 참여한 미션
+        List<ResponseMissionDto> findLatelyParticipationMission = missionService.findLatelyParticipationMission(userId);
+        // 최근 작성한 미션
+        List<ResponseMissionDto> findLatelyCreatedMission = missionService.findLatelyCreatedMission(userId);
+        // 최근 작성한 게시글
+
+        model.addAttribute("participationMission", findLatelyParticipationMission);
+        model.addAttribute("createdMission", findLatelyCreatedMission);
+
         return "contents/user/myPage";
+    }
+
+    /**
+     * 마이페이지 - 나의 참여 미션
+     * @param customUserDetails
+     * @param model
+     * @param pageable
+     * @param search
+     * @return
+     */
+    @GetMapping("/user/mypage/my-participation-mission")
+    public String myParticipationMission(@AuthenticationPrincipal CustomUserDetails customUserDetails, Model model, @PageableDefault(size = 8) Pageable pageable, @RequestParam(required = false, defaultValue = "") String search){
+        String userId = customUserDetails.getLoginUserDto().getUserId();
+
+        List<ResponseMissionDto> findLatelyParticipationMission = missionService.findLatelyParticipationMission(userId);
+
+        model.addAttribute("participationMission", findLatelyParticipationMission);
+
+        return "contents/user/myParticipationMission";
     }
 
     /**
