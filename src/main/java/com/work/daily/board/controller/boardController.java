dd8e6838bde1c94111addfc85990e1f419e80ecb
youngwooooo <dlddudn02@gmail.com>
2022-02-17 21:20:45 +0900

    1. 커뮤니티(게시판) 컨트롤러, 서비스 생성

---
+++ src/main/java/com/work/daily/board/controller/boardController.java
@@ -0,0 +1,18 @@
+package com.work.daily.board.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class boardController {
+
+    @GetMapping("/boards")
+    public String boards(){
+
+        return "/contents/board/boards";
+    }
+}
