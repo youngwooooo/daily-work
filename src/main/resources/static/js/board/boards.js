1e37a7707cd06d5bad532e706622b4a5563fe3e2
youngwooooo <dlddudn02@gmail.com>
2022-02-21 22:07:17 +0900

    1. 커뮤니티(전체게시글) 화면 구성 수정 2. 게시글 상세 페이지 이동 js 추가

--- src/main/resources/static/js/board/boards.js
+++ src/main/resources/static/js/board/boards.js
@@ -5,4 +5,11 @@ $(function(){
         location.href = "/board";
     });
 
+
+    // 게시글 제목 클릭 시, 게시글 상세페이지 이동
+    $(".board-list-div .table tbody td:nth-child(3)").on("click", function(){
+        var boardSeq = $(this).siblings(".board-num").text();
+
+        location.href = "/board/" + boardSeq;
+    });
 });
