88d67399d5469b19fee0d3bc64db254605428632
youngwooooo <dlddudn02@gmail.com>
2022-02-21 22:02:34 +0900

    1. findOneBoard() 추가 : 게시글 단건(상세) 조회

--- src/main/java/com/work/daily/domain/repository/custom/BoardRepositoryCustom.java
+++ src/main/java/com/work/daily/domain/repository/custom/BoardRepositoryCustom.java
@@ -1,10 +1,11 @@
 package com.work.daily.domain.repository.custom;
 
-import com.work.daily.board.dto.ResponseBoardDto;
 import com.work.daily.domain.entity.Board;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BoardRepositoryCustom {
     List<Board> findAllBoard();
+    Optional<Board> findOneBoard(long boardSeq);
 }
--- src/main/java/com/work/daily/domain/repository/custom/impl/BoardRepositoryCustomImpl.java
+++ src/main/java/com/work/daily/domain/repository/custom/impl/BoardRepositoryCustomImpl.java
@@ -3,12 +3,14 @@ package com.work.daily.domain.repository.custom.impl;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.work.daily.domain.entity.Board;
 import com.work.daily.domain.entity.QBoard;
+import com.work.daily.domain.entity.QBoardFile;
 import com.work.daily.domain.entity.QUser;
 import com.work.daily.domain.repository.custom.BoardRepositoryCustom;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Repository
@@ -16,8 +18,13 @@ public class BoardRepositoryCustomImpl implements BoardRepositoryCustom {
 
     private final JPAQueryFactory jpaQueryFactory;
     QBoard qBoard = QBoard.board;
+    QBoardFile qBoardFile = QBoardFile.boardFile;
     QUser qUser = QUser.user;
 
+    /**
+     * 전체 게시글 조회
+     * @return
+     */
     @Override
     public List<Board> findAllBoard() {
         return jpaQueryFactory
@@ -27,4 +34,23 @@ public class BoardRepositoryCustomImpl implements BoardRepositoryCustom {
                         .and(qBoard.temporaryYn.eq("N"))
                 ).fetch();
     }
+
+    /**
+     * 게시글 단건(상세) 조회
+     * @param boardSeq
+     * @return
+     */
+    @Override
+    public Optional<Board> findOneBoard(long boardSeq) {
+        return Optional.ofNullable(jpaQueryFactory
+                .selectFrom(qBoard)
+                .innerJoin(qBoard.user, qUser).fetchJoin()
+                .leftJoin(qBoard.boardFileList, qBoardFile).fetchJoin()
+                .where(qBoard.delYn.eq("N")
+                        .and(qBoard.temporaryYn.eq("N"))
+                        .and(qBoard.boardSeq.eq(boardSeq))
+                ).fetchOne());
+    }
+
+
 }
