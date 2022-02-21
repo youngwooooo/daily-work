3a4e03db221ee8de3cced5a2a5e9f3a0a1fabf6a
youngwooooo <dlddudn02@gmail.com>
2022-02-21 22:03:17 +0900

    1. BoardFile Entity 생성 : 게시글 첨부파일 데이터를 관리하는 Entity

---
+++ src/main/java/com/work/daily/domain/entity/BoardFile.java
@@ -0,0 +1,48 @@
+package com.work.daily.domain.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "TB_BOARD_FILE")
+public class BoardFile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "BOARD_FILE_SEQ_INCREASE")
+    @Column(columnDefinition = "varchar(20) comment '게시글파일번호'")
+    private long fileSeq;
+
+    @Column(columnDefinition = "varchar(20) comment '게시글파일순서'")
+    private long fileOrder;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "BOARD_SEQ")
+    @JsonBackReference
+    private Board board;
+
+    @Column(columnDefinition = "varchar(300) comment '게시글파일원본파일명'")
+    private String fileOriginNm;
+
+    @Column(columnDefinition = "varchar(300) comment '게시글파일서버파일명'")
+    private String fileServerNm;
+
+    @Column(columnDefinition = "varchar(200) comment '게시글파일경로'")
+    private String fileUploadPath;
+
+    @Column(columnDefinition = "number comment '게시글파일크기'")
+    private long fileSize;
+
+    @Column(columnDefinition = "varchar(5) comment '이미지여부'")
+    private String imageYn;
+
+    @Column(columnDefinition = "varchar(30) comment '게시글파일등록일자'")
+    private LocalDateTime insDtm;
+
+}
