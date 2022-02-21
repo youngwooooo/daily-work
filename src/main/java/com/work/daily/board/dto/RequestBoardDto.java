3f55e99c9a389ae9188ecd1e78037a395ba40af8
youngwooooo <dlddudn02@gmail.com>
2022-02-21 22:00:34 +0900

    1. filed, toEntity() 추가

--- src/main/java/com/work/daily/board/dto/RequestBoardDto.java
+++ src/main/java/com/work/daily/board/dto/RequestBoardDto.java
@@ -1,5 +1,8 @@
 package com.work.daily.board.dto;
 
+import com.work.daily.domain.BoardType;
+import com.work.daily.domain.entity.Board;
+import com.work.daily.domain.entity.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,4 +13,23 @@ import lombok.ToString;
 @ToString
 @NoArgsConstructor
 public class RequestBoardDto {
+
+    private User user;
+    private String boardNm;
+    private String boardDesc;
+    private String delYn;
+    private String temporaryYn;
+    private BoardType boardType;
+
+    public Board toEntity(){
+        return Board.builder()
+                .user(user)
+                .boardNm(boardNm)
+                .boardDesc(boardDesc)
+                .delYn(delYn)
+                .temporaryYn(temporaryYn)
+                .boardType(boardType)
+                .build();
+    }
+
 }
