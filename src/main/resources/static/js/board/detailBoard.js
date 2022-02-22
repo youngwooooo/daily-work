4d3f3e71a89f01a2f4e14a0c7487d95c34bad360
youngwooooo <dlddudn02@gmail.com>
2022-02-22 21:44:01 +0900

    1. 게시글 상세 화면 디자인 수정 2. 파일 다운로드 js 로직 구현

---
+++ src/main/resources/static/js/board/detailBoard.js
@@ -0,0 +1,29 @@
+$(function(){
+
+    // 파일 용량 포맷
+    $(".file-size").each(function(){
+        $(this).text("(" + fileSizePrint($(this).text()) + ")");
+    });
+
+    // 파일 다운로드
+    $(".board-file-list .list-group-item").on("click", function(){
+        var fileSeq = $(this).find("input[name='fileSeq']").val();
+        location.href = "/boardFile/download/" + fileSeq;
+    });
+
+});
+
+function fileSizePrint(fileSize){
+     var result = "";
+     if(fileSize < 1024){
+        result = fileSize + "B";
+     }else if(fileSize < 1024 * 1024) {
+        result = parseInt (fileSize / 1024) + "KB";
+     }else if(fileSize < 1024 * 1024 * 1024) {
+        result = parseInt (fileSize / (1024 * 1024)) + "M";
+     }else {
+        result = parseInt (fileSize / (1024 * 1024 * 1024)) + "G";
+     }
+
+     return result;
+}
