fd65541b9fb0001e0653ca24f914217e823b2d7b
youngwooooo <dlddudn02@gmail.com>
2022-02-23 22:11:55 +0900

    1. 댓글 화면 구성 추가

--- src/main/resources/static/js/board/detailBoard.js
+++ src/main/resources/static/js/board/detailBoard.js
@@ -14,6 +14,45 @@ $(function(){
         location.href = "/boardFile/download/" + fileSeq;
     });
 
+    // textarea 높이 자동 조절
+    autosize($(".board-comment-content"));
+
+    // textarea 글자 수 제한하기
+    $(document).on('keydown keyup', ".board-comment-content" , function () {
+        var content = $(this).val(); // 글자수
+
+        $(".text-count").text(content.length);
+        $(".text-count-div").css("display", "block");
+
+    });
+
+    $(".board-comment-create-reply").on("click", function(){
+        var userNm = $("#userNm").val();
+
+        var str = '<li class="board-comment-reply">'
+                +  '<div class="create-board-comment-form-div">'
+                +       '<div class="create-board-comment-form justify-content-between">'
+                +           '<span>'+ userNm + '</span>'
+                +           '<div class="text-count-div">'
+                +               '<span class="text-count"></span>/<span>200</span>'
+                +           '</div>'
+                +       '</div>'
+                +       '<textarea class="board-comment-content" placeholder="답글을 입력하세요" maxlength="200"></textarea>'
+                +       '<div class="board-comment-buttons">'
+                +           '<button type="button" class="btn btn-primary">등록</button>'
+                +       '</div>'
+                +   '</div>'
+                + '</li>';
+
+        if($(".board-comment-reply").length > 0){
+            $(".board-comment-reply").remove();
+        }else {
+            $(this).parents(".board-comment").after(str);
+        }
+
+
+    });
+
 });
 
 function fileSizePrint(fileSize){
