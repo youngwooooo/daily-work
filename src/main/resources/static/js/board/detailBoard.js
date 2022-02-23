94421a404589095ed62c601ba914912b24f285ad
youngwooooo <dlddudn02@gmail.com>
2022-02-23 19:28:52 +0900

    1. 카테고리 확인 삭제 2. 삭제/수정 툴팁 추가 3. 파일 다운로드 아이콘 추가

--- src/main/resources/static/js/board/detailBoard.js
+++ src/main/resources/static/js/board/detailBoard.js
@@ -1,13 +1,16 @@
 $(function(){
 
+    // 툴팁 활성화
+    $('[data-toggle="tooltip"]').tooltip()
+
     // 파일 용량 포맷
     $(".file-size").each(function(){
         $(this).text("(" + fileSizePrint($(this).text()) + ")");
     });
 
     // 파일 다운로드
-    $(".board-file-list .list-group-item").on("click", function(){
-        var fileSeq = $(this).find("input[name='fileSeq']").val();
+    $(".board-file-list .list-group-item .file-download").on("click", function(){
+        var fileSeq = $(this).siblings("input[name='fileSeq']").val();
         location.href = "/boardFile/download/" + fileSeq;
     });
 
