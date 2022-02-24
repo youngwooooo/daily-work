e0fc34656ef7576c0eeb4eb7b5a4e11a38c638c2
youngwooooo <dlddudn02@gmail.com>
2022-02-24 22:23:40 +0900

    1. deleteBoard() 추가 : 게시글 삭제여부 N으로 변경 2. modifyBoard() 추가: 게시글 제목,내용,카테고리 변경

--- src/main/java/com/work/daily/apiserver/board/BoardApiController.java
+++ src/main/java/com/work/daily/apiserver/board/BoardApiController.java
@@ -47,5 +47,62 @@ public class BoardApiController {
         return new ResponseEntity<>(ResponseDto.builder().status(HttpStatus.CREATED.value()).data(result).message("게시글 등록이 완료되었습니다.").build(), HttpStatus.CREATED);
     }
 
+    /**
+     * 게시글 수정
+     * @description 1. 게시글 제목, 내용, 카테고리 수정
+     *              2. 첨부파일 수정 시, 삭제 및 업로드
+     * @param boardSeq
+     * @param requestBoardDto
+     * @param files
+     * @param fileSeqList
+     * @return
+     * @throws IOException
+     */
+    @PatchMapping("/board/{boardSeq}")
+    public ResponseEntity<ResponseDto> modifyBoard(@PathVariable("boardSeq") long boardSeq
+                                                    , @RequestPart(value = "requestBoardDto") RequestBoardDto requestBoardDto
+                                                    , @RequestPart(value = "files", required = false) List<MultipartFile> files
+                                                    , @RequestPart(value = "fileSeqList", required = false) List<Long> fileSeqList
+    ) throws IOException {
+        log.info("requestBoardDto : " + requestBoardDto.toString());
+        if(files != null){
+            for(MultipartFile file : files){
+                log.info("업로드 파일 : " + file.getOriginalFilename());
+            }
+        }
+
+        log.info("fileSeqList Size : " + fileSeqList.size());
+        if(fileSeqList != null){
+            for(int i=0; i<fileSeqList.size(); i++){
+                log.info("fileSeq : " + fileSeqList.get(i));
+            }
+        }
+
+        String result = boardService.modify(boardSeq, requestBoardDto, files, fileSeqList);
+
+        if(!ReturnResult.SUCCESS.getValue().equals(result)){
+            return new ResponseEntity<>(ResponseDto.builder().status(HttpStatus.BAD_REQUEST.value()).data(result).message("게시글 수정에 실패하였습니다. 다시 시도해주세요.").build(), HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(ResponseDto.builder().status(HttpStatus.OK.value()).data(result).message("게시글 수정이 완료되었습니다.").build(), HttpStatus.OK);
+    }
+
+    /**
+     * 게시글 삭제
+     * @description 게시글의 삭제여부를 N으로 수정
+     * @param boardSeq
+     * @return
+     */
+    @DeleteMapping("/board/{boardSeq}")
+    public ResponseEntity<ResponseDto> deleteBoard(@PathVariable("boardSeq") long boardSeq){
+
+        String result = boardService.delete(boardSeq);
+
+        if(!ReturnResult.SUCCESS.getValue().equals(result)){
+            return new ResponseEntity<>(ResponseDto.builder().status(HttpStatus.BAD_REQUEST.value()).data(result).message("게시글 삭제가 실패하였습니다. 다시 시도해주세요.").build(), HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(ResponseDto.builder().status(HttpStatus.OK.value()).data(result).message("게시글 삭제가 완료되었습니다.").build(), HttpStatus.OK);
+    }
 
 }
