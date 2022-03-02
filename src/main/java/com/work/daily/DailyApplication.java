87d3fba44ae91f22ccdca9ed28cbcb632d5b58a9
youngwooooo <dlddudn02@gmail.com>
2022-03-02 22:31:32 +0900

    1 @EnableScheduling 추가 : @Scheduler 사용을 위함

--- src/main/java/com/work/daily/DailyApplication.java
+++ src/main/java/com/work/daily/DailyApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Optional;
 
+@EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
 public class DailyApplication {
