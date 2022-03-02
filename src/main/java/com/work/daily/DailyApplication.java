862d34afe2d4518721b3fa9f71ba114ccf5123e1
youngwooooo <dlddudn02@gmail.com>
2022-03-02 20:05:31 +0900

    1. @Slf4j 삭제

--- src/main/java/com/work/daily/DailyApplication.java
+++ src/main/java/com/work/daily/DailyApplication.java
@@ -1,7 +1,6 @@
 package com.work.daily;
 
 import com.work.daily.login.auth.CustomUserDetails;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +12,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Optional;
 
-@Slf4j
 @EnableJpaAuditing
 @SpringBootApplication
 public class DailyApplication {
