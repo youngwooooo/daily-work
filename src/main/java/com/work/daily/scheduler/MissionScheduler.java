3ba4e96543787bc16edccbe4215de00ffb1471ae
youngwooooo <dlddudn02@gmail.com>
2022-03-03 22:44:55 +0900

    1. closeMission -> closeMissionAndMissionState() 이름 변경

--- src/main/java/com/work/daily/scheduler/MissionScheduler.java
+++ src/main/java/com/work/daily/scheduler/MissionScheduler.java
@@ -17,14 +17,16 @@ public class MissionScheduler {
     private final MissionService missionService;
 
     /**
-     * 미션 마감여부 N -> Y로 변경하는 스케쥴러
-     * @description 매일 오전 6시에 미션종료일이 오늘보다 이전인 미션들의 마감여부를 Y로 수정한다.
+     * 미션 마감여부 N -> Y로 변경 후, 미션 현황 승인여부 N -> Y로 변경하는 스케쥴러
+     * @description 1. 매일 오전 6시에 스케쥴러 실행
+     *              2. 미션종료일이 오늘보다 이전인 미션들의 마감여부를 Y로 수정한다.
+     *              3. 마감된 미션의 미션 현황 중 반려되지 않은 미션 현황(제출 미션)의 승인야부를 Y로 수정한다.
      */
     @Scheduled(cron = "0 0 6 * * *")
-    public void closeMission(){
-        log.info("스케쥴러 실행");
+    public void closeMissionAndMissionState(){
+        log.info("MissionScheduler :: closeMissionAndMissionState 실행");
         LocalDateTime now = LocalDateTime.now().truncatedTo(ChronoUnit.DAYS);
-        missionService.closeMission(now);
+        missionService.closeMissionAndMissionState(now);
 
     }
 }
